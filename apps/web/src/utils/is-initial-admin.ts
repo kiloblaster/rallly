1c09a764c10fad0e71d09f805ec32cdf25be8613
Luke Vella <me@lukevella.com>
2026-04-20 12:36:51 -0300

    🚀 Make initial admin check case insensitve

--- apps/web/src/utils/is-initial-admin.ts
+++ apps/web/src/utils/is-initial-admin.ts
@@ -2,6 +2,7 @@ import "server-only";
 
 export function isInitialAdmin(email: string) {
   return (
-    process.env.INITIAL_ADMIN_EMAIL && email === process.env.INITIAL_ADMIN_EMAIL
+    process.env.INITIAL_ADMIN_EMAIL &&
+    email.toLowerCase() === process.env.INITIAL_ADMIN_EMAIL.toLowerCase()
   );
 }
